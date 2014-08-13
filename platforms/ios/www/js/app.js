eda16ac22998d1715e74652bab77fbf23dce5255
Chad Timmins <chadtimmins@gmail.com>
2014-08-13 13:03:39 -0700

    Added a login/new user page

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,12 +1,29 @@
 /*  js/app.js  */
-     
-$(document).on('deviceready', function(){
+console.log("loaded app.js");     
+$(document).on('ready', function(){
     console.log("deviceready");
+
     $(document).on('mobileinit', function(){
         console.log("jqm has been loaded");
+        //prepend nav panel on each page load
+        $(document).one("pagebeforecreate", function(){
+            $.get('templates/navpanel.html', function(template){
+                var panel = template;
+                $.mobile.pageContainer.prepend(panel);
+                $('#navpanel').panel();
+            });
+        });
+        //log current page and html on each page change
+        $(document).on("pagecontainerbeforeshow", function(event, data){
+            var pageId = $('body').pagecontainer('getActivePage').attr('id');
+            console.log("current page: " + pageId);
+         });
+
         app.renderHomeView();
     })
-    $.getScript('js/lib/jquery/jquery.mobile-1.4.3.min.js');
+    
+    $('body').append('<script type="text/javascript" src="js/lib/jquery/jquery.mobile-1.4.3.min.js"></script>');
+
 });
 
 var app = {
@@ -14,24 +31,81 @@ var app = {
     renderHomeView: function(){
         console.log("rendering home view");
         $(document).ready(function(){
-            $.get('templates/homeTemplate.html', function(template){
-                $('#home').css("background", "url(./img/root-background-4.jpg)")
-                $('#home').html(template).trigger("create");
-            });        
+            // $.get('templates/homeTemplate.html', function(template){
+            //     //$('#home').css("background", "url(./img/root-background-4.jpg)")
+            //     $('#home').html(template).trigger("create");
+            // });        
 
         });
         
     },
 
+
     renderMapView: function(){
         console.log("rendering map view");
-        $('body').pagecontainer('load', 'templates/maps.html', {
-            role: "page",
-            id: "map"
-        });
+
+        // function onMapLoad() { 
+        //     console.log("got to onLoad");
+        //     if (1) {
+        //         // load the google api
+        //         var fileref=document.createElement('script'); 
+        //         fileref.setAttribute("type","text/javascript"); 
+        //         fileref.setAttribute("src",
+        //             "http://maps.googleapis.com/maps/api/js?sensor=true&callback=" + "getGeolocation");
+        //         document.getElementsByTagName("head")[0]. appendChild(fileref);
+        //     } 
+        //     else {
+        //         alert("Must be connected to the Internet");
+        //     } 
+        // };
+
+        // function getGeolocation() {
+        //     // get the user's gps coordinates and display map
+        //     console.log("got to getGeolocation"); 
+        //     var options = {
+        //         maximumAge: 3000, 
+        //         timeout: 5000, 
+        //         enableHighAccuracy: true
+        //     }; 
+        //     navigator.geolocation.getCurrentPosition(loadMap, geoError, options);
+        // };
+
+        // function loadMap(position) {
+        //     console.log("got to loadMap");
+        //     var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        //     var myOptions = { 
+        //         zoom: 8,
+        //         center: latlng,
+        //         mapTypeId: google.maps.MapTypeId.ROADMAP
+        //     };
+        //     var mapObj = $('#map_canvas');
+        //     var map = new google.maps.Map(mapObj[0], myOptions);
+
+        //     var marker = new google.maps.Marker({
+        //         position: latlng,
+        //         map: map,
+        //         title: "You"
+        //     });
+                
+        // };
+
+        // function geoError(error){
+        //     alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+        // };
     }
 };
 
- $(document).on("pagecontainerchange", function(event, data){
-     console.log("current page: " + $.mobile.activePage.attr('id'));
- });
+// $(document).one("pagebeforecreate", function(){
+//     $.get('templates/navpanel.html', function(template){
+//         var panel = template;
+//         $.mobile.pageContainer.prepend(panel);
+//         $('#navpanel').panel();
+//     });
+// });
+
+//  $(document).on("pagecontainerbeforeshow", function(event, data){
+//     var pageId = $.mobile.activePage.attr('id')
+//     console.log("current page: " + pageId);
+//     console.log("");
+//     console.log($('body').html());
+//  });
---
+++ platforms/ios/www/plugins/pro.fing.cordova.gapreload/gapreload.js
@@ -0,0 +1,60 @@
+cordova.define("pro.fing.cordova.gapreload.gapreload", function(require, exports, module) { /*jshint browser:true, devel:true */
+/*global device */
+
+function getXML(path, callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', path + '.xml');
+  request.overrideMimeType('text/xml');
+  request.onreadystatechange = function () {
+    if (request.readyState === 4) { callback(request.responseXML); }
+  };
+  request.send();
+}
+
+function getParams(xml) {
+  // It must return a true array so that we can use `forEach` later.
+  return xml ? [].slice.call(xml.querySelectorAll('param[name][value]')) : [];
+}
+
+function mergeParams(xml, defaults) {
+  var merged = {};
+  getParams(xml).forEach(function (param) {
+    // `param.name` and `param.value` don't work.
+    var name = param.getAttribute('name');
+    var value = param.getAttribute('value');
+    // `value` has to be a replaced variable and can only be merged once.
+    if (merged[name] || !value || value === '$' + name) { return; }
+    defaults[name] = merged[name] = value;
+  });
+  return defaults;
+}
+
+function getOrigin(host, port) { return 'http://' + host + ':' + port + '/'; }
+
+function addScript(url) {
+  var scriptToAdd = document.createElement('script');
+  scriptToAdd.src = url;
+  var script = document.getElementsByTagName('script')[0];
+  script.parentNode.insertBefore(scriptToAdd, script);
+}
+
+getXML('gapreload', function (config) {
+  config = mergeParams(config, { SERVER_PORT: 8000, LIVERELOAD_PORT: 35729 });
+  // First loading only, and skip it directly if accessed from desktop.
+  if (!/^http/.test(location)) {
+    // Wait until the server runs and confirm to use GapReload.
+    if (!confirm('Do you want to use GapReload?')) { return; }
+    var serverOrigin = getOrigin(config.SERVER_HOST, config.SERVER_PORT);
+    var contentPath = /\/www\/.+$/.exec(location)[0];
+    return document.addEventListener('deviceready', function () {
+      var platform = device.platform.toLowerCase();
+      // Use `replace` so that it won't break the Android back button.
+      location.replace(serverOrigin + platform + contentPath);
+    }, false);
+  }
+  var liveReloadHost = config.LIVERELOAD_HOST || config.SERVER_HOST;
+  var liveReloadOrigin = getOrigin(liveReloadHost, config.LIVERELOAD_PORT);
+  addScript(liveReloadOrigin + 'livereload.js?snipver=1');
+});
+
+});
