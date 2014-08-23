b80bba46eb39d9cc8b7cbf0ea29376795d7f9d2a
Chad Timmins <chadtimmins@gmail.com>
2014-08-22 21:31:34 -0700

    update user account working

--- www/js/account.js
+++ www/js/account.js
@@ -5,11 +5,17 @@ var account = {
 	initialize: function(){
 		console.log("initializing account");
 		$(document).on("pagebeforecreate", "#account", function(event, ui){
+			account.renderAccount(app.userData);
+
 			$('#logout_button').on("touchstart", function(){
 				console.log("account logout");
 				app.logout();
 			});
-			account.renderAccount(app.userData);
+
+			$('#saveAccountDetails').on("touchstart", function(){
+				account.updateAccount();
+			});
+			
 		});
 	},
 
@@ -17,17 +23,57 @@ var account = {
 		console.log("rendering account");
 		$('#accountsettings_name').val(userData["Name"]);
         $('#accountsettings_email').val(userData["Email"]);
+        $('#accountsettings_phone').val(userData["Phone"]);
         $('#accountsettings_address').val(userData["Address"]);
         $('#accountsettings_city').val(userData["City"]);
         $('#accountsettings_state').val(userData["State"]);
         $('#accountsettings_zip').val(userData["Zip"]);
         $('#accountsettings_country').val(userData["Country"]);
         $('#accountsettings_acreage').val(userData["Acreage"]);
+        $('select#water_source').val(userData["Water Source"]);
+        //$('select#water_source').selectmenu('refresh');
+        $('select#irrigation_method').val(userData["Irrigation System"]);
+        //$('select#irrigation_method').selectmenu('refresh');
+        $('select#soil_type').val(userData["Soil Type"]);
+        //$('select#soil_type').selectmenu('refresh');
 	},
 
 	updateAccount: function(){
+        var out = {
+            Name: $('#accountsettings_name').val(),
+            Address: $('#accountsettings_address').val(),
+            City: $('#accountsettings_city').val(),
+            State: $('#accountsettings_state').val(),
+            Country: $('#accountsettings_country').val(),
+            Zip: $('#accountsettings_zip').val(),
+            Phone: $('#accountsettings_phone').val(),
+            Acreage: $('#accountsettings_acreage').val(),
+            IrrSystem: $('#irrigation_method').val(),
+            WaterSource: $('#water_source').val(),
+            SoilType: $('#soil_type').val(),
+            Mode: 'UpdateUserDetails'
+        };
 
+        $.getJSON(handler, out, function(returnVal){
+            console.log("update " + returnVal);
+            if(returnVal == "Success"){
+            	//var email = window.localStorage.getItem("Email");
+	        	//var pass = window.localStorage.getItem("Password");
+				//app.login(email, pass);
+				app.userData["Name"] = $('#accountsettings_name').val();
+				app.userData["Email"] = $('#accountsettings_email').val();
+				app.userData["Phone"] = $('#accountsettings_phone').val();
+				app.userData["Address"] = $('#accountsettings_address').val();
+				app.userData["City"] = $('#accountsettings_city').val();
+				app.userData["State"] = $('#accountsettings_state').val();
+				app.userData["Zip"] = $('#accountsettings_zip').val();
+				app.userData["Country"] = $('#accountsettings_country').val();
+				app.userData["Acreage"] = $('#accountsettings_acreage').val();
+				app.userData["Water Source"] = $('#water_source').val();
+				app.userData["Irrigation System"] = $('#irrigation_method').val();
+            	app.userData["Soil Type"] = $('select#soil_type').val();
+            }
+        });
 	}
-
 }
 
--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 /*  js/app.js  */
 
 var handler = 'http://farmperfect.com/libs/php/manager.php';
-var thisApp;
+
 
 $(document).on('deviceready', function(){
     console.log("deviceready");
@@ -24,7 +24,7 @@ $(document).on('deviceready', function(){
 var app = {
 
     initialize: function(){
-        thisApp = this;
+
 
         //prepend nav panel and try login.  only triggered once
         $(document).one("pagecontainerbeforechange", function(e, ui){
@@ -38,20 +38,21 @@ var app = {
             //retrieve user credentials from local storage and try login
             var email = window.localStorage.getItem("Email");
             var pass = window.localStorage.getItem("Password");
-            console.log("email: " + email);
-            console.log("password: " + pass);
+
             if(email != null && pass != null) {
-                thisApp.login(email, pass);
+                app.login(email, pass);
             }
             else{
-                thisApp.logout();
+                app.logout();
             }
+
             e.preventDefault();
-            console.log(thisApp.userData);
+            console.log(app.userData);
         });
 
         $(document).on("pagecontainerbeforeshow", function(e, ui){
             console.log("current page: " + ui.toPage.attr('id'));
+            app.currentPage = ui.toPage.attr('id');
         })
 
         $(document).on('pagebeforecreate', '#dashboard', function(event, ui){
@@ -66,43 +67,37 @@ var app = {
                 console.log("login clicked");
                 var  email = $('#email').val();
                 var pass = $('#pass').val();
-                console.log("email: " + email);
-                console.log("pw: " + pass);
-                thisApp.login(email, pass);
+                app.login(email, pass);
             });
         
-        });
-
-        // $(document).on('pagebeforecreate', '#account', function(event, ui){
-        //     console.log("showing account");
-        //     //thisApp.renderAccount(returnVal);
-        //     $('#account_button').click(function(){
-        //         thisApp.renderAccount(returnVal);
-        //     });
-        //     $('#logout_button').click(function(){
-        //         thisApp.logout();
-        //     });
-        // });      
+        });      
     },
 
     login: function(email, pass){
         console.log("trying to login");
         $.getJSON(handler, {Email: email, Password: pass, Mode: "GetUserDetails"}, function(returnVal){
-            console.log("request returned");
+
             if(returnVal != "Fail"){
                 //store user credentials upon successful login
                 window.localStorage.setItem("Email", email);
                 window.localStorage.setItem("Password", pass);
-                
+                //store userData on successful login
+                app.userData = returnVal
                 //initialize account
                 account.initialize();
-                //change page to dashboard                
-                $('body').pagecontainer("change", "#dashboard", {allowSamePageTransition: true});
+                //change page to dashboard
+                // if(app.currentPage == "account"){
+                //     $('body').pagecontainer("change", "templates/account.html", {allowSamePageTransition: true});       
+                // }
+                // else{
+                //     $('body').pagecontainer("change", "#dashboard", {allowSamePageTransition: true});     
+                // }                
+                $('body').pagecontainer("change", "#dashboard", {allowSamePageTransition: true});     
 
 
             }
             else{
-                thisApp.logout();
+                app.logout();
             }
         });    
     },
@@ -115,64 +110,16 @@ var app = {
         console.log("rendering map view");
     },
 
-    renderAccount: function(userData){
-        $(document).off("pagecontainerbeforeshow").on("pagecontainerbeforeshow", function(event, data){
-            $('#accountsettings_name').val(userData["Name"]);
-            $('#accountsettings_email').val(userData["Email"]);
-            $('#accountsettings_address').val(userData["Address"]);
-            $('#accountsettings_city').val(userData["City"]);
-            $('#accountsettings_state').val(userData["State"]);
-            $('#accountsettings_zip').val(userData["Zip"]);
-            $('#accountsettings_country').val(userData["Country"]);
-            $('#accountsettings_acreage').val(userData["Acreage"]);
-
-            $('#logout_button').click(function(){
-                thisApp.logout();
-            });
-            $('#saveAccountDetails').click(function(){
-                thisApp.updateAccountDetails();
-            });
-        });
-    },
-
-    updateAccountDetails: function(){
-        var out = {
-            Name: $('#accountsettings_name').val(),
-            Address: $('#accountsettings_address').val(),
-            City: $('#accountsettings_city').val(),
-            State: $('#accountsettings_state').val(),
-            Country: $('#accountsettings_country').val(),
-            Zip: $('#accountsettings_zip').val(),
-            Phone: $('#accountsettings_phone').val(),
-            Acreage: $('#accountsettings_acreage').val(),
-            IrrSystem: $('#irrigation_method').val(),
-            WaterSource: $('#water_source').val(),
-            SoilType: $('#soil_type').val(),
-            Mode: 'UpdateUserDetails'
-        };
-
-        $.getJSON(handler, out, function(returnVal){
-            console.log("update successful");
-        });
-    },
-
     logout: function(){
         $('body').pagecontainer("change", "#login", {allowSamePageTransition: true});
         console.log("logging out");
         //clear user credentials
         window.localStorage.clear();
         //show login screen
-        
-        
-        //$('body').pagecontainer("change");
-        //$(document).trigger('createpage');
-       
+        app.userData = null;
     },
 
-    userData: {
-        Name: "chad",
-        address: "123 yes lane"
-    }
+    userData: null
 };
 
 
